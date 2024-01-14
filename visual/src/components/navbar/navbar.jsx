05ab48d01edaa02cd4770c79807385c2461d4360
oliveira533 <go609127@gmail.com>
2024-01-14 15:22:56 -0300

    Começando a fazer a navbar

---
+++ visual/src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './navbar.css';
+
+function NavBar(){
+    <nav>
+        <a></a>
+    </nav>
+}
+
+export default NavBar;
