49e54f9d3555f015f2287fd0e7a2f8d02f1c2008
oliveira533 <go609127@gmail.com>
2024-01-21 13:26:15 -0300

    Barra de navegação finalizada

--- visual/src/components/navbar/navbar.jsx
+++ visual/src/components/navbar/navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './navbar.css';
 import { FaHome }from "react-icons/fa";
+import { FaGear }from "react-icons/fa6";
+import { IoIosStats }from "react-icons/io";
 
 function NavBar(){
     return(
         <nav className='nav'>
-            <a href='#'><FaHome /></a>
+            <a href='#' className='item'><FaHome className='item-icon first-icon'/></a>
+            <a href='#' className='item'><FaGear className='item-icon'/></a>
+            <a href='#' className='item'><IoIosStats className='item-icon'/></a>
         </nav>
     )
 }
